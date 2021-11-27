7ab09ca216ea9855a07e150570ee860b5751b29c
Nikita <emper137137@gmail.com>
2021-11-27 13:38:36 +1000

    login view update

--- client/src/components/featured/LogAndRegView/LogAndRegView.js
+++ client/src/components/featured/LogAndRegView/LogAndRegView.js
@@ -1,9 +1,67 @@
-import React from 'react';
+import { useState } from 'react';
+import {
+    Container,
+    Form,
+    FormGroup,
+    Input,
+    Label,
+    Button
+} from 'reactstrap';
 
 export const LogAndRegView = () => {
+    const [email, setEmail] = useState(null);
+    const [password, setPassword] = useState(null);
+
+    // Login logic
+    const login = () => {
+        console.log(`logged as ${email}:${password}`);
+    }
+
+    // Register logic
+    const register = () => {
+        console.log(`registered as ${email}:${password}`);
+    }
     return (
-        <p>
-          Please, sign in or log in  
-        </p>
+        <Container style={{margin: '5rem auto'}}>
+            <Form inline>
+                <FormGroup floating>
+                    <Input
+                        id="inputEmail"
+                        name="email"
+                        placeholder="Email"
+                        type="email"
+                        onChange={({ target }) => setEmail(target.value)}
+                    />
+                    <Label for="inputEmail">Email</Label>
+                </FormGroup>
+                {' '}
+                <FormGroup floating>
+                    <Input
+                        id="inputPassword"
+                        name="password"
+                        placeholder="Password"
+                        type="password"
+                        onChange={({ target }) => setPassword(target.value)}
+                    />
+                    <Label for="inputPassword">Password</Label>
+                </FormGroup>
+                {' '}
+                <div style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    gap: '1rem'
+                }}>
+                    <Button 
+                        style={{width: '6rem'}}
+                        onClick={login}
+                    >Login</Button>
+                    <p>or</p>
+                    <Button 
+                        style={{width: '6rem'}}
+                        onClick={register}
+                    >Register</Button>
+                </div>
+            </Form>
+        </Container>
     )
 }
--- client/src/components/featured/MainView/MainView.js
+++ client/src/components/featured/MainView/MainView.js
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export const MainView = () => {
     return (
         <p>

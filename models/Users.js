e20b9e533a514cf642559d5d8d4d06d06068ca62
Nikita <emper137137@gmail.com>
2021-11-27 17:17:12 +1000

    users db & backend func

---
+++ models/Users.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true,
+        default: Date.now
+    }
+});
+
+module.exports = User = mongoose.model('user', UserSchema);
---
+++ routes/api/users.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+const router = express.Router();
+
+// User model
+const User = require('../../models/Users');
+
+// @ route      GET api/users
+// @ desc       GET user with the specified email
+// @ access     Public
+
+router.get('/', (req, res) => {
+    User
+        .find({ email: req.body.email })
+        .then(user => res.json(user))
+        .catch(err => console.log(err));
+});
+
+// @ route      POST api/users
+// @ desc       POST new user
+// @ access     Public
+
+router.post('/', (req, res) => {
+    const newUser = new User({
+        email: req.body.email,
+        password: req.body.password
+    });
+    newUser
+        .save()
+        .then(user => res.json(user))
+        .catch(err => console.log(err));
+});
+
+module.exports = router;

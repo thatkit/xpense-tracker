558e26acbb61df113f3b89f7f2ac5b4fa80710f6
Nikita <emper137137@gmail.com>
2022-01-12 12:44:38 +1000

    slight store refinement

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,13 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import listReducer from './slices/currentListSlice';
-import itemReducer from './slices/currentItemSlice';
 import apiReducer from './slices/apiSlice';
 import uiReducer from './slices/uiSlice';
 
 export const store = configureStore({
     reducer: {
         currentList: listReducer,
-        currentItem: itemReducer,
         api: apiReducer,
         ui: uiReducer
     }

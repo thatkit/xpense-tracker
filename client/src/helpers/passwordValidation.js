88ff7f5323bc5b3ddada8860135b6b93315db913
Nikita <emper137137@gmail.com>
2022-01-17 08:33:49 +1000

    npm package.json change

--- client/src/helpers/passwordValidation.js
+++ client/src/helpers/passwordValidation.js
@@ -1,12 +1,12 @@
-import passwordValidator from 'password-validator';
+// import passwordValidator from 'password-validator';
 
-const schema = new passwordValidator();
+// const schema = new passwordValidator();
 
-schema
-    .is().min(4)
-    .is().max(16)
-    .has().digits(1)
-    .has().letters(1)
-    .has().not().spaces();
+// schema
+//     .is().min(4)
+//     .is().max(16)
+//     .has().digits(1)
+//     .has().letters(1)
+//     .has().not().spaces();
 
-export default schema;
+// export default schema;
